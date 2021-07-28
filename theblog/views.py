8fd11b890839c4bc146700c78484711ebe3c7bef
nickl98 <nicklodde98@gmail.com>
2021-07-28 18:03:50 +0000

    Bug fix

--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def blog(request):
-    return render(request, 'blog.html', {
+# def blog(request):
+#     return render(request, 'blog.html', {
 
-    })
+#     })
+
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog.html'
