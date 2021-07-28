7d6611dc02bfec9e537c7a488963253fbe9d4113
nickl98 <nicklodde98@gmail.com>
2021-07-28 16:47:12 +0000

    Created user_ratings application and its model

---
+++ user_ratings/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from products.models import Product
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Review(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=False, default='')
+    review = models.CharField(max_length=1000, blank=False, default='')
+    RATING_CHOICES = (
+        ("1", '1'),
+        ("2", '2'),
+        ("3", '3'),
+        ("4", '4'),
+        ("5", '5'),
+    )
+    rating = models.CharField(max_length=10,
+                              choices=RATING_CHOICES,
+                              default="3")
+    date = models.DateTimeField(default=timezone.now, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ user_ratings/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render, get_list_or_404, HttpResponse, redirect
+
+# Create your views here.
