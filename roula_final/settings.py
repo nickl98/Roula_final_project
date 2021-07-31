1a6b62ccdae472db6b75b6795612cef5cdd6946f
nickl98 <nicklodde98@gmail.com>
2021-07-31 03:06:38 +0000

    bug fix

--- roula_final/settings.py
+++ roula_final/settings.py
@@ -198,7 +198,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 
 
