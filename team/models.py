a9300cf3d39d3d3cd7ac329ccce5611e2d5f36db
nickl98 <nicklodde98@gmail.com>
2021-08-06 15:01:39 +0000

    Lets hope this works

--- team/models.py
+++ team/models.py
@@ -9,7 +9,7 @@ class Team(models.Model):
     years_experience = models.CharField(max_length=2)
     quote = models.CharField(max_length=254, null=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
