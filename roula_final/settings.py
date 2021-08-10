35afb2fdd47575f94f414e55f6e8ff6795a71c20
nickl98 <nicklodde98@gmail.com>
2021-08-10 02:29:19 +0000

    html error fixes

--- roula_final/settings.py
+++ roula_final/settings.py
@@ -1,8 +1,9 @@
 import os
+import dj_database_url
 if os.path.exists("env.py"):
     import env
     # If statement so no need to comment out import env when pushing to Heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,7 +124,7 @@ WSGI_APPLICATION = 'roula_final.wsgi.application'
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))  
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     print("Postgres URL not found, using sqlite instead")
@@ -132,7 +133,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }  
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
